e5c3b3eb65f750d730ce691dcd4c683720664831
Linnea Eriksson <erikssonlinneam@gmail.com>
2015-09-24 10:59:03 +0200

    JAg la till extra funktionalitet i console appen nu

--- Consoletest/Consoletest/Program.cs
+++ Consoletest/Consoletest/Program.cs
@@ -10,13 +10,18 @@ namespace Consoletest
     {
         static void Main(string[] args)
         {
-            //Tjena viktoria här har jag ändrat koden och skrivit en kommentar %
+            //Tjena viktoria och Cecilia här har jag ändrat koden och skrivit en kommentar %
             Console.WriteLine("Vem är snyggast Pontus eller anton? svara [A] [P]");
             if (Console.ReadLine() == "A")
-                {
+            {
                 Console.WriteLine("Tror jag inte på att du tycker!");
                 Console.ReadKey();
-                   }
+            }
+            else
+            {
+                Console.WriteLine("HaHa! Jag visste det!");
+                Console.ReadKey();
+            }
         }
     }
 }
